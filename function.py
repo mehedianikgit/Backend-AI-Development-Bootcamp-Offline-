eb962c10c4dbba7d7648c005628fc9a7bb7e2591
mehedianikgit <mehedianikgit@github.com>
2026-07-18 11:57:53 +0600

    function

---
+++ function.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    sum = a+b
+    return sum
+
+def subtract (x,y):
+    sub = x-y
+    return sub
+
+def devide (x,y):
+    dev = x/y
+    return dev
+
+x = 100
+y = 40
+
+print (add(x,y))
+print (subtract(x,y))
+print (devide(x,y))
